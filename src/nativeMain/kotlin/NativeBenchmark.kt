e33313b444933cc07ec770c694b0df32ac315b9b
kevinlim17 <kevinlim17@icloud.com>
2025-03-15 21:53:31 +0900

    Add: Benchmark classes for JVM and native target

---
+++ src/nativeMain/kotlin/NativeBenchmark.kt
@@ -0,0 +1,23 @@
+package test
+
+import kotlinx.benchmark.*
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(BenchmarkTimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = BenchmarkTimeUnit.SECONDS)
+@Measurement(timeUnit = BenchmarkTimeUnit.SECONDS)
+class NativeBenchmark {
+    private var data = 0.0
+
+    @Setup
+    fun setUp() {
+        data = 3.0
+    }
+
+    @Benchmark
+    fun sqrtBenchmark(): Double = kotlin.math.sqrt(data)
+
+    @Benchmark
+    fun cosBenchmark(): Double = kotlin.math.cos(data)
+}
