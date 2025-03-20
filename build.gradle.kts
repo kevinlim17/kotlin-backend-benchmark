82c07dd874401b6daa18af3339651b1365596137
kevinlim17 <kevinlim17@icloud.com>
2025-03-20 15:22:10 +0900

    Add: optimization option for native targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,26 @@ kotlin {
     }
 
     /** Native Targets */
-    macosX64()
-    linuxX64()
-    macosArm64()
-    mingwX64()
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        /** For adding optimization option */
+        binaries {
+            all {
+                freeCompilerArgs += listOf("-opt")
+            }
+        }
+    }
 
     /** Apply Multiplatform Source Hierarchy */
     applyDefaultHierarchyTemplate()
